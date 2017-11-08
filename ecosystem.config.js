827b2b4f74196208795006cfdcc56989d0232d0f
Roy Myers <myersrdev@gmail.com>
2017-11-08 17:26:07 -0500

    adding creation script for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name   : "exampleNodeExpress",
+    script : "./index.js",
+    instances: 1,
+    env: {
+       "NODE_ENV" : "development",
+    },
+    env_production: {
+       "NODE_ENV" : "production"
+    }
+  }]
+}
